4d8eadbcb73f9970d28be45ca83434e585268001
Bret Quigley <jellopuddingstick@gmail.com>
2021-01-23 08:32:12 -0600

    Upgrade server to 1.17

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-meson x --buildtype release --strip -Db_lto=true -Dprebuilt_server=scrcpy-server-v1.16 \
+meson x --buildtype release --strip -Db_lto=true -Dprebuilt_server=scrcpy-server-v1.17 \
   && ninja -Cx \
   && sudo ninja -Cx install
